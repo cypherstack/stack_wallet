7144baf02e888fc7145511b4a81f831708020ebe
Marco <marco@cypherstack.com>
2022-08-26 20:28:26 +0800

    include git commit build history

--- scripts/android/build_all.sh
+++ scripts/android/build_all.sh
@@ -6,7 +6,7 @@ mkdir build
 
 (cd ../../crypto_plugins/flutter_liblelantus/scripts/android && ./build_all.sh ) &
 (cd ../../crypto_plugins/flutter_libepiccash/scripts/android && ./install_ndk.sh && ./build_all.sh )  &
-(cd ../../crypto_plugins/flutter_libmonero/scripts/android/ && ./install_ndk.sh && ./build_monero_all.sh && ./copy_monero_deps.sh ) &
+(cd ../../crypto_plugins/flutter_libmonero/scripts/android/ && ./build_all.sh  ) &
 
 wait
 echo "Done building"
