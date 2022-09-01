550752f11b80bbe7e6ddcfcca26256eaa56456fc
Marco <marco@cypherstack.com>
2022-09-01 12:29:33 +0800

    ubuntu 20

--- scripts/linux/build_secure_storage_deps.sh
+++ scripts/linux/build_secure_storage_deps.sh
@@ -5,16 +5,17 @@ mkdir build
 # Build JsonCPP
 cd build
 git clone https://github.com/open-source-parsers/jsoncpp.git
-git checkout 8190e061bc2d95da37479a638aa2c9e483e58ec6
+git checkout 48d2a69d47bbf92337a09fc1672e1bad39fdde86
 cd jsoncpp
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=release -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=ON -DARCHIVE_INSTALL_DIR=. -G "Unix Makefiles" ..
 make -j$(nproc)
 
 cd $LINUX_DIRECTORY
 # Build libSecret
-# sudo apt install libgirepository1.0-dev valac xsltproc gi-docgen docbook-xsl
+# sudo apt install meson libgirepository1.0-dev valac xsltproc gi-docgen docbook-xsl
+#pip3 install --user meson --upgrade
 # pip3 install --user gi-docgen
 cd build
 git clone https://gitlab.gnome.org/GNOME/libsecret.git
