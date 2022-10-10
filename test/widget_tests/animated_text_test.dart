3c50077ed1b892060c50da755a1caf547b8ec1fa
Likho <likhojiba@gmail.com>
2022-10-10 12:30:35 +0200

    WIP: widget tests, animated text test

---
+++ test/widget_tests/animated_text_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stackwallet/utilities/theme/light_colors.dart';
+import 'package:stackwallet/utilities/theme/stack_colors.dart';
+import 'package:stackwallet/widgets/animated_text.dart';
+
+void main() {
+  testWidgets("Widget displays first word in strings list",
+      (widgetTester) async {
+    const animatedText = AnimatedText(
+        stringsToLoopThrough: [
+          "Calculating",
+          "Calculating.",
+          "Calculating..",
+          "Calculating...",
+        ],
+        style: TextStyle(
+          color: null,
+          fontWeight: FontWeight.w500,
+          fontSize: 14,
+        ));
+    await widgetTester.pumpWidget(
+      MaterialApp(
+        theme: ThemeData(
+          extensions: [
+            StackColors.fromStackColorTheme(LightColors()),
+          ],
+        ),
+        home: const Material(
+          child: animatedText,
+        ),
+      ),
+    );
+
+    expect(find.text("Calculating"), findsOneWidget);
+  });
+}
