20c34a185b8b5ef572d9b0d4a51f61962fa45816
Likho <likhojiba@gmail.com>
2022-10-19 12:09:34 +0200

    Trade Card test

---
+++ test/widget_tests/trade_card_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:stackwallet/utilities/theme/light_colors.dart';
+import 'package:stackwallet/utilities/theme/stack_colors.dart';
+
+void main() {}
