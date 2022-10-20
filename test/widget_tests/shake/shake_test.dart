bb40d4b16593c94d9f5cc46fb147fd02c87bf70a
Likho <likhojiba@gmail.com>
2022-10-20 12:49:45 +0200

    WIP: Widget tests

---
+++ test/widget_tests/shake/shake_test.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stackwallet/utilities/theme/light_colors.dart';
+import 'package:stackwallet/utilities/theme/stack_colors.dart';
