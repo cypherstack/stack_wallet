712b33833b39fe603bd11b9f333549ca2f9283a2
ryleedavis <rylee@cypherstack.com>
2022-11-02 09:02:53 -0600

    stack experience dialog for desktop advanced settings

--- lib/pages_desktop_specific/home/advanced_settings/advanced_settings.dart
+++ lib/pages_desktop_specific/home/advanced_settings/advanced_settings.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:stackwallet/pages/stack_privacy_calls.dart';
+import 'package:stackwallet/pages_desktop_specific/home/advanced_settings/debug_info_dialog.dart';
+import 'package:stackwallet/pages_desktop_specific/home/advanced_settings/stack_privacy_dialog.dart';
+import 'package:stackwallet/providers/global/prefs_provider.dart';
 import 'package:stackwallet/utilities/assets.dart';
 import 'package:stackwallet/utilities/text_styles.dart';
 import 'package:stackwallet/utilities/theme/stack_colors.dart';
+import 'package:stackwallet/widgets/custom_buttons/draggable_switch_button.dart';
 import 'package:stackwallet/widgets/rounded_white_container.dart';
-
-import '../../../pages/settings_views/global_settings_view/advanced_views/debug_view.dart';
-import '../../../providers/global/prefs_provider.dart';
-import '../../../widgets/custom_buttons/draggable_switch_button.dart';
 
 class AdvancedSettings extends ConsumerStatefulWidget {
   const AdvancedSettings({Key? key}) : super(key: key);
@@ -109,62 +108,68 @@ class _AdvancedSettings extends ConsumerState<AdvancedSettings> {
                     ),
 
                     /// TODO: Make a dialog popup
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Stack Experience",
-                                style:
-                                    STextStyles.desktopTextExtraSmall(context)
-                                        .copyWith(
-                                            color: Theme.of(context)
-                                                .extension<StackColors>()!
-                                                .textDark),
-                                textAlign: TextAlign.left,
-                              ),
-                              Text(
-                                "Easy Crypto",
-                                style: STextStyles.desktopTextExtraExtraSmall(
-                                    context),
-                              ),
-                            ],
-                          ),
-                          const StackPrivacyButton(),
-                        ],
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Divider(
-                        thickness: 0.5,
-                      ),
-                    ),
-
-                    /// TODO: Make a dialog popup
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Debug info",
-                            style: STextStyles.desktopTextExtraSmall(context)
-                                .copyWith(
-                                    color: Theme.of(context)
-                                        .extension<StackColors>()!
-                                        .textDark),
-                            textAlign: TextAlign.left,
-                          ),
-                          ShowLogsButton(),
-                        ],
-                      ),
-                    ),
+                    Consumer(builder: (_, ref, __) {
+                      final externalCalls = ref.watch(
+                        prefsChangeNotifierProvider
+                            .select((value) => value.externalCalls),
+                      );
+                      return Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Stack Experience",
+                                  style:
+                                      STextStyles.desktopTextExtraSmall(context)
+                                          .copyWith(
+                                              color: Theme.of(context)
+                                                  .extension<StackColors>()!
+                                                  .textDark),
+                                  textAlign: TextAlign.left,
+                                ),
+                                Text(
+                                  externalCalls ? "Easy crypto" : "Incognito",
+                                  style: STextStyles.desktopTextExtraExtraSmall(
+                                      context),
+                                ),
+                              ],
+                            ),
+                            const StackPrivacyButton(),
+                          ],
+                        ),
+                      );
+                    }),
                   ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Divider(
+                    thickness: 0.5,
+                  ),
+                ),
+
+                /// TODO: Make a dialog popup
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Debug info",
+                        style: STextStyles.desktopTextExtraSmall(context)
+                            .copyWith(
+                                color: Theme.of(context)
+                                    .extension<StackColors>()!
+                                    .textDark),
+                        textAlign: TextAlign.left,
+                      ),
+                      ShowLogsButton(),
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -181,6 +186,17 @@ class StackPrivacyButton extends ConsumerWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    Future<void> changePrivacySettings() async {
+      await showDialog<dynamic>(
+        context: context,
+        useSafeArea: false,
+        barrierDismissible: true,
+        builder: (context) {
+          return StackPrivacyDialog();
+        },
+      );
+    }
+
     return SizedBox(
       width: 84,
       height: 37,
@@ -189,10 +205,11 @@ class StackPrivacyButton extends ConsumerWidget {
             .extension<StackColors>()!
             .getPrimaryEnabledButtonColor(context),
         onPressed: () {
-          Navigator.of(context).pushNamed(
-            StackPrivacyCalls.routeName,
-            arguments: false,
-          );
+          // Navigator.of(context).pushNamed(
+          //   StackPrivacyCalls.routeName,
+          //   arguments: false,
+          // );
+          changePrivacySettings();
         },
         child: Text(
           "Change",
@@ -210,6 +227,17 @@ class ShowLogsButton extends ConsumerWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    Future<void> viewDebugLogs() async {
+      await showDialog<dynamic>(
+        context: context,
+        useSafeArea: false,
+        barrierDismissible: true,
+        builder: (context) {
+          return const DebugInfoDialog();
+        },
+      );
+    }
+
     return SizedBox(
       width: 101,
       height: 37,
@@ -218,7 +246,8 @@ class ShowLogsButton extends ConsumerWidget {
             .extension<StackColors>()!
             .getPrimaryEnabledButtonColor(context),
         onPressed: () {
-          Navigator.of(context).pushNamed(DebugView.routeName);
+          //
+          viewDebugLogs();
         },
         child: Text(
           "Show logs",
