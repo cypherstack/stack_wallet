b333253287c44beb6bc86428cdfe3f55eb938df9
julian <julian@cypherstack.com>
2022-11-19 15:12:08 -0600

    reduce minimum window height

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ void main() async {
 
   if (Util.isDesktop) {
     setWindowTitle('Stack Wallet');
-    setWindowMinSize(const Size(1220, 1100));
+    setWindowMinSize(const Size(1220, 1000));
     setWindowMaxSize(Size.infinite);
   }
 
