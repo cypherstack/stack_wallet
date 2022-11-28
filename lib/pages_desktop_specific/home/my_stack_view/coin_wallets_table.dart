d7cd5cb8a9c556a29f6ef8e273a3f72503b523e3
julian <julian@cypherstack.com>
2022-11-28 15:21:18 -0600

    desktop wallets table hover effects

--- lib/pages_desktop_specific/home/my_stack_view/coin_wallets_table.dart
+++ lib/pages_desktop_specific/home/my_stack_view/coin_wallets_table.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stackwallet/pages_desktop_specific/home/my_stack_view/wallet_view/desktop_wallet_view.dart';
 import 'package:stackwallet/utilities/constants.dart';
 import 'package:stackwallet/utilities/theme/stack_colors.dart';
+import 'package:stackwallet/widgets/rounded_container.dart';
 import 'package:stackwallet/widgets/wallet_info_row/wallet_info_row.dart';
 
 class CoinWalletsTable extends ConsumerWidget {
@@ -24,8 +25,10 @@ class CoinWalletsTable extends ConsumerWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 16,
+          // horizontal: 20,
+          // vertical: 16,
+          horizontal: 6,
+          vertical: 6,
         ),
         child: Column(
           children: [
@@ -36,18 +39,82 @@ class CoinWalletsTable extends ConsumerWidget {
                     const SizedBox(
                       height: 32,
                     ),
-                  WalletInfoRow(
-                    walletId: walletIds[i],
-                    onPressed: () async {
-                      await Navigator.of(context).pushNamed(
-                        DesktopWalletView.routeName,
-                        arguments: walletIds[i],
-                      );
-                    },
+                  Stack(
+                    children: [
+                      WalletInfoRow(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 14,
+                          vertical: 10,
+                        ),
+                        walletId: walletIds[i],
+                      ),
+                      Positioned.fill(
+                        child: WalletRowHoverOverlay(
+                          onPressed: () async {
+                            await Navigator.of(context).pushNamed(
+                              DesktopWalletView.routeName,
+                              arguments: walletIds[i],
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class WalletRowHoverOverlay extends StatefulWidget {
+  const WalletRowHoverOverlay({
+    Key? key,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final VoidCallback onPressed;
+
+  @override
+  State<WalletRowHoverOverlay> createState() => _WalletRowHoverOverlayState();
+}
+
+class _WalletRowHoverOverlayState extends State<WalletRowHoverOverlay> {
+  late final VoidCallback onPressed;
+
+  bool _hovering = false;
+
+  @override
+  void initState() {
+    onPressed = widget.onPressed;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (_) {
+        setState(() {
+          _hovering = true;
+        });
+      },
+      onExit: (_) {
+        setState(() {
+          _hovering = false;
+        });
+      },
+      child: GestureDetector(
+        onTap: onPressed,
+        child: AnimatedOpacity(
+          duration: const Duration(milliseconds: 100),
+          opacity: _hovering ? 0.1 : 0,
+          child: RoundedContainer(
+            color:
+                Theme.of(context).extension<StackColors>()!.buttonBackSecondary,
+          ),
         ),
       ),
     );
