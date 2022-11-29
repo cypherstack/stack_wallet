f635f474c6b73a2abe1ccdd63e4ba6c818953d03
sneurlax <sneurlax@gmail.com>
2022-11-29 16:48:26 -0600

    update prebuild.sh to create stub wallet test parameter files

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -2,5 +2,36 @@
 KEYS=../lib/external_api_keys.dart
 if ! test -f "$KEYS"; then
     echo 'prebuild.sh: creating template lib/external_api_keys.dart file'
-    printf 'const kChangeNowApiKey = "";\nconst kSimpleSwapApiKey = "";' > $KEYS
+    printf 'const kChangeNowApiKey = "";\nconst kSimpleSwapApiKey = "";\n' > $KEYS
+fi
+
+# Create template wallet test parameter files if they don't already exist
+BWTP=../test/services/coins/bitcoin/bitcoin_wallet_test_parameters.dart
+if ! test -f "$BWTP"; then
+    echo 'prebuild.sh: creating template test/services/coins/bitcoin/bitcoin_wallet_test_parameters.dart file'
+    printf 'const TEST_MNEMONIC = "";\nconst ROOT_WIF = "";\nconst NODE_WIF_84 = "";\n' > $BWTP
+fi
+
+BCWTP=../test/services/coins/bitcoincash/bitcoincash_wallet_test_parameters.dart
+if ! test -f "$BCWTP"; then
+    echo 'prebuild.sh: creating template test/services/coins/bitcoincash/bitcoincash_wallet_test_parameters.dart file'
+    printf 'const TEST_MNEMONIC = "";\nconst ROOT_WIF = "";\nconst NODE_WIF_84 = "";\n' > $BCWTP
+fi
+
+DWTP=../test/services/coins/dogecoin/dogecoin_wallet_test_parameters.dart
+if ! test -f "$DWTP"; then
+    echo 'prebuild.sh: creating template test/services/coins/dogecoin/dogecoin_wallet_test_parameters.dart file'
+    printf 'const TEST_MNEMONIC = "";\nconst ROOT_WIF = "";\nconst NODE_WIF_84 = "";\n' > $DWTP
+fi
+
+NWTP=../test/services/coins/namecoin/namecoin_wallet_test_parameters.dart
+if ! test -f "$NWTP"; then
+    echo 'prebuild.sh: creating template test/services/coins/namecoin/namecoin_wallet_test_parameters.dart file'
+    printf 'const TEST_MNEMONIC = "";\nconst ROOT_WIF = "";\nconst NODE_WIF_84 = "";\n' > $NWTP
+fi
+
+FWTP=../test/services/coins/firo/firo_wallet_test_parameters.dart
+if ! test -f "$FWTP"; then
+    echo 'prebuild.sh: creating template test/services/coins/firo/firo_wallet_test_parameters.dart file'
+    printf 'const TEST_MNEMONIC = "";\nconst ROOT_WIF = "";\nconst NODE_WIF_84 = "";\n' > $FWTP
 fi
