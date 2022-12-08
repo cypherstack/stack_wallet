9bd7a3d0d24c890cec39f3aebdf9d2011ec70dd6
julian <julian@cypherstack.com>
2022-12-08 11:58:22 -0600

    disable tx notification

--- lib/pages/home_view/home_view.dart
+++ lib/pages/home_view/home_view.dart
@@ -8,19 +8,15 @@ import 'package:epicmobile/pages/settings_views/network_settings_view/network_se
 import 'package:epicmobile/pages/settings_views/settings_view.dart';
 import 'package:epicmobile/pages/wallet_view/wallet_view.dart';
 import 'package:epicmobile/providers/global/wallet_provider.dart';
-import 'package:epicmobile/providers/tx_count_on_startup_state_provider.dart';
 import 'package:epicmobile/providers/ui/home_view_index_provider.dart';
 import 'package:epicmobile/services/event_bus/events/global/refresh_percent_changed_event.dart';
-import 'package:epicmobile/services/event_bus/events/global/updated_in_background_event.dart';
 import 'package:epicmobile/services/event_bus/events/global/wallet_sync_status_changed_event.dart';
 import 'package:epicmobile/services/event_bus/global_event_bus.dart';
 import 'package:epicmobile/utilities/assets.dart';
-import 'package:epicmobile/utilities/messages.dart';
 import 'package:epicmobile/utilities/text_styles.dart';
 import 'package:epicmobile/utilities/theme/stack_colors.dart';
 import 'package:epicmobile/widgets/background.dart';
 import 'package:epicmobile/widgets/custom_buttons/app_bar_icon_button.dart';
-import 'package:epicmobile/widgets/rounded_container.dart';
 import 'package:epicmobile/widgets/stack_dialog.dart';
 import 'package:event_bus/event_bus.dart';
 import 'package:flutter/material.dart';
@@ -141,27 +137,27 @@ class _HomeViewState extends ConsumerState<HomeView> {
     }
 
     eventBus = widget.eventBus ?? GlobalEventBus.instance;
-    eventBus.on<UpdatedInBackgroundEvent>().listen((event) async {
-      final count = ref.read(txCountOnStartUpProvider.state).state ?? 0;
-      final newCount = ref.read(walletProvider)!.txCount;
-      if (count < newCount) {
-        ref.read(txCountOnStartUpProvider.state).state = newCount;
-        await Messages.of(context).show(
-          context: context,
-          widget: RoundedContainer(
-            color: Theme.of(context).extension<StackColors>()!.coal,
-            child: Center(
-              child: Text(
-                "New transaction",
-                style: STextStyles.bodySmallBold(context).copyWith(
-                  color: Theme.of(context).extension<StackColors>()!.textMedium,
-                ),
-              ),
-            ),
-          ),
-        );
-      }
-    });
+    // eventBus.on<UpdatedInBackgroundEvent>().listen((event) async {
+    //   final count = ref.read(txCountOnStartUpProvider.state).state ?? 0;
+    //   final newCount = ref.read(walletProvider)!.txCount;
+    //   if (count < newCount) {
+    //     ref.read(txCountOnStartUpProvider.state).state = newCount;
+    //     await Messages.of(context).show(
+    //       context: context,
+    //       widget: RoundedContainer(
+    //         color: Theme.of(context).extension<StackColors>()!.coal,
+    //         child: Center(
+    //           child: Text(
+    //             "New transaction",
+    //             style: STextStyles.bodySmallBold(context).copyWith(
+    //               color: Theme.of(context).extension<StackColors>()!.textMedium,
+    //             ),
+    //           ),
+    //         ),
+    //       ),
+    //     );
+    //   }
+    // });
 
     _syncStatusSubscription =
         eventBus.on<WalletSyncStatusChangedEvent>().listen(
