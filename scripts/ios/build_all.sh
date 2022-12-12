5aa6dc3c1ad08a318de28ca25af3e5ffc8576fbf
ryleedavis <rylee@cypherstack.com>
2022-12-12 16:54:54 -0700

    removed liblelantus and libmonero

--- scripts/ios/build_all.sh
+++ scripts/ios/build_all.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-(cd ../../crypto_plugins/flutter_liblelantus/scripts/ios && ./build_all.sh ) &
 (cd ../../crypto_plugins/flutter_libepiccash/scripts/ios && ./build_all.sh )  &
-(cd ../../crypto_plugins/flutter_libmonero/scripts/ios/ && ./build_all.sh  ) &
 
 wait
 echo "Done building"
