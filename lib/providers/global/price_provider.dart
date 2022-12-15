f0b6668348c9ccd5a6c5076415d6eb882356e2bd
julian <julian@cypherstack.com>
2022-12-15 11:49:58 -0600

    fix price not updating on currency change

--- lib/providers/global/price_provider.dart
+++ lib/providers/global/price_provider.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:epicpay/providers/global/prefs_provider.dart';
 import 'package:epicpay/services/price_service.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 int _count = 0;
 
@@ -15,5 +15,5 @@ final priceAnd24hChangeNotifierProvider =
 
   final currency =
       ref.watch(prefsChangeNotifierProvider.select((value) => value.currency));
-  return PriceService(currency);
+  return PriceService(currency)..start(true);
 });
