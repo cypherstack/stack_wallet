27bb0cf1c925d1e6a805d2b6d39984a610c7e794
julian <julian@cypherstack.com>
2022-12-21 11:13:41 -0600

    place holder featured paynyms

---
+++ lib/utilities/featured_paynyms.dart
@@ -0,0 +1,7 @@
+abstract class FeaturedPaynyms {
+  // TODO: replace with actual values
+  static const String samouraiWalletDevFund =
+      "PM8TJYkuSdYXJnwDBq8ChfinfXv3srxhQrx3eoEwbSw51wMjdo9JJ2DsycwT3gt3zHQ7cV1grvabMmmf1Btj6fY7tgkgSz9B8MZuR3kjYfgMLMURJCXN";
+  static const String stackWallet =
+      "PM8TJYkuSdYXJnwDBq8ChfinfXv3srxhQrx3eoEwbSw51wMjdo9JJ2DsycwT3gt3zHQ7cV1grvabMmmf1Btj6fY7tgkgSz9B8MZuR3kjYfgMLMURJCXN";
+}
