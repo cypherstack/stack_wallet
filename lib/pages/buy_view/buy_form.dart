2d6447eedd46a19d27f77c684d20cd56a3b34018
sneurlax <sneurlax@gmail.com>
2023-01-11 11:49:59 -0600

    add fiat selection view

--- lib/pages/buy_view/buy_form.dart
+++ lib/pages/buy_view/buy_form.dart
@@ -2,9 +2,11 @@ import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stackwallet/models/buy/response_objects/crypto.dart';
+import 'package:stackwallet/models/buy/response_objects/fiat.dart';
 import 'package:stackwallet/models/buy/response_objects/pair.dart';
-import 'package:stackwallet/pages/buy_view/buy_coin_selection/crypto_selection_view.dart';
+import 'package:stackwallet/pages/buy_view/sub_widgets/crypto_selection_view.dart';
 import 'package:stackwallet/pages/buy_view/sub_widgets/fiat_crypto_toggle.dart';
+import 'package:stackwallet/pages/buy_view/sub_widgets/fiat_selection_view.dart';
 import 'package:stackwallet/providers/providers.dart';
 import 'package:stackwallet/utilities/constants.dart';
 import 'package:stackwallet/utilities/enums/coin_enum.dart';
@@ -57,12 +59,6 @@ class _BuyFormState extends ConsumerState<BuyForm> {
     }
   }
 
-  void selectFiatCrypto() async {
-    // await Future<void>.delayed(const Duration(milliseconds: 300));
-    //
-    // Navigator.of(context, rootNavigator: true).pop();
-  }
-
   void cryptoFieldOnChanged(String value) async {
     if (_cryptoFocusNode.hasFocus) {
       final newCryptoAmount = Decimal.tryParse(value);
@@ -80,8 +76,8 @@ class _BuyFormState extends ConsumerState<BuyForm> {
     }
   }
 
-  void selectCryptoCrypto() async {
-    final fromTicker = ref.read(exchangeFormStateProvider).fromTicker ?? "-";
+  void selectCrypto() async {
+    final fromTicker = ref.read(buyFormStateProvider).fromTicker ?? "-";
     // ref.read(estimatedRateExchangeFormProvider).from?.ticker ?? "-";
 
     // if (walletInitiated &&
@@ -91,18 +87,18 @@ class _BuyFormState extends ConsumerState<BuyForm> {
     // }
 
     List<Crypto> coins;
-    switch (ref.read(currentExchangeNameStateProvider.state).state) {
-      // case ChangeNowExchange.exchangeName:
-      //   coins = ref.read(availableChangeNowCurrenciesProvider).coins;
-      //   break;
-      // case SimpleSwapExchange.exchangeName:
-      //   coins = ref
-      //       .read(availableSimpleswapCurrenciesProvider)
-      //       .floatingRateCurrencies;
-      //   break;
-      default:
-        coins = [];
-    }
+    // switch (ref.read(currentExchangeNameStateProvider.state).state) {
+    //   // case ChangeNowExchange.exchangeName:
+    //   //   coins = ref.read(availableChangeNowCurrenciesProvider).coins;
+    //   //   break;
+    //   // case SimpleSwapExchange.exchangeName:
+    //   //   coins = ref
+    //   //       .read(availableSimpleswapCurrenciesProvider)
+    //   //       .floatingRateCurrencies;
+    //   //   break;
+    //   default:
+    coins = [];
+    // }
 
     await _showFloatingCryptoSelectionSheet(
         coins: coins,
@@ -251,6 +247,146 @@ class _BuyFormState extends ConsumerState<BuyForm> {
     }
   }
 
+  void selectFiat() async {
+    final fromTicker = ref.read(buyFormStateProvider).fromTicker ?? "-";
+    // ref.read(estimatedRateExchangeFormProvider).from?.ticker ?? "-";
+
+    // if (walletInitiated &&
+    //     fromTicker.toLowerCase() == coin!.ticker.toLowerCase()) {
+    //   // do not allow changing away from wallet coin
+    //   return;
+    // }
+
+    List<Crypto> coins;
+    // switch (ref.read(currentExchangeNameStateProvider.state).state) {
+    // // case ChangeNowExchange.exchangeName:
+    // //   coins = ref.read(availableChangeNowCurrenciesProvider).coins;
+    // //   break;
+    // // case SimpleSwapExchange.exchangeName:
+    // //   coins = ref
+    // //       .read(availableSimpleswapCurrenciesProvider)
+    // //       .floatingRateCurrencies;
+    // //   break;
+    //   default:
+    coins = [];
+    // }
+  }
+
+  Future<void> _showFloatingFiatSelectionSheet({
+    required List<Fiat> fiats,
+    required String excludedTicker,
+    required String fromTicker,
+    required void Function(Fiat) onSelected,
+  }) async {
+    _fiatFocusNode.unfocus();
+    _cryptoFocusNode.unfocus();
+
+    List<Pair> allPairs;
+
+    // switch (ref.read(currentExchangeNameStateProvider.state).state) {
+    // // case ChangeNowExchange.exchangeName:
+    // //   allPairs = ref.read(availableChangeNowCurrenciesProvider).pairs;
+    // //   break;
+    // // case SimpleSwapExchange.exchangeName:
+    // //   allPairs = ref.read(exchangeFormStateProvider).exchangeType ==
+    // //           ExchangeRateType.fixed
+    // //       ? ref.read(availableSimpleswapCurrenciesProvider).fixedRatePairs
+    // //       : ref.read(availableSimpleswapCurrenciesProvider).floatingRatePairs;
+    // //   break;
+    //   default:
+    allPairs = [];
+    // }
+
+    List<Pair> availablePairs;
+    if (fromTicker.isEmpty ||
+        fromTicker == "-" ||
+        excludedTicker.isEmpty ||
+        excludedTicker == "-") {
+      availablePairs = allPairs;
+    } else if (excludedTicker == fromTicker) {
+      availablePairs = allPairs
+          .where((e) => e.from == excludedTicker)
+          .toList(growable: false);
+    } else {
+      availablePairs =
+          allPairs.where((e) => e.to == excludedTicker).toList(growable: false);
+    }
+
+    final List<Fiat> tickers = fiats.where((e) {
+      if (excludedTicker == fromTicker) {
+        return e.ticker != excludedTicker &&
+            availablePairs.where((e2) => e2.to == e.ticker).isNotEmpty;
+      } else {
+        return e.ticker != excludedTicker &&
+            availablePairs.where((e2) => e2.from == e.ticker).isNotEmpty;
+      }
+    }).toList(growable: false);
+
+    final result = isDesktop
+        ? await showDialog<Crypto?>(
+            context: context,
+            builder: (context) {
+              return DesktopDialog(
+                maxHeight: 700,
+                maxWidth: 580,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 32,
+                          ),
+                          child: Text(
+                            "Choose a fiat with which to pay",
+                            style: STextStyles.desktopH3(context),
+                          ),
+                        ),
+                        const DesktopDialogCloseButton(),
+                      ],
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 32,
+                          right: 32,
+                          bottom: 32,
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: RoundedWhiteContainer(
+                                padding: const EdgeInsets.all(16),
+                                borderColor: Theme.of(context)
+                                    .extension<StackColors>()!
+                                    .background,
+                                child: FiatSelectionView(
+                                  coins: tickers,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            })
+        : await Navigator.of(context).push(
+            MaterialPageRoute<dynamic>(
+              builder: (_) => FiatSelectionView(
+                coins: tickers,
+              ),
+            ),
+          );
+
+    if (mounted && result is Fiat) {
+      onSelected(result);
+    }
+  }
+
   String? _fetchIconUrlFromTicker(String? ticker) {
     if (ticker == null) return null;
 
@@ -364,12 +500,15 @@ class _BuyFormState extends ConsumerState<BuyForm> {
             }
           },
           onChanged: cryptoFieldOnChanged,
-          onButtonTap: selectCryptoCrypto,
+          onButtonTap: selectCrypto,
           isWalletCoin: isWalletCoin(coin, true),
           image: _fetchIconUrlFromTicker(ref
               .watch(buyFormStateProvider.select((value) => value.fromTicker))),
           ticker: ref
               .watch(buyFormStateProvider.select((value) => value.fromTicker)),
+        ),
+        SizedBox(
+          height: isDesktop ? 20 : 12,
         ),
         Row(
           crossAxisAlignment: CrossAxisAlignment.end,
@@ -385,6 +524,33 @@ class _BuyFormState extends ConsumerState<BuyForm> {
         ),
         SizedBox(
           height: isDesktop ? 10 : 4,
+        ),
+        BuyTextField(
+          controller: _fiatController,
+          focusNode: _fiatFocusNode,
+          textStyle: STextStyles.smallMed14(context).copyWith(
+            color: Theme.of(context).extension<StackColors>()!.textDark,
+          ),
+          buttonColor:
+              Theme.of(context).extension<StackColors>()!.buttonBackSecondary,
+          borderRadius: Constants.size.circularBorderRadius,
+          background:
+              Theme.of(context).extension<StackColors>()!.textFieldDefaultBG,
+          onTap: () {
+            if (_fiatController.text == "-") {
+              _fiatController.text = "";
+            }
+          },
+          onChanged: fiatFieldOnChanged,
+          onButtonTap: selectFiat,
+          isWalletCoin: isWalletCoin(coin, true),
+          image: _fetchIconUrlFromTicker(ref
+              .watch(buyFormStateProvider.select((value) => value.fromTicker))),
+          ticker: ref
+              .watch(buyFormStateProvider.select((value) => value.fromTicker)),
+        ),
+        SizedBox(
+          height: isDesktop ? 20 : 12,
         ),
         Row(
           crossAxisAlignment: CrossAxisAlignment.end,
@@ -422,7 +588,7 @@ class _BuyFormState extends ConsumerState<BuyForm> {
             }
           },
           onChanged: fiatFieldOnChanged,
-          onButtonTap: selectFiatCrypto,
+          onButtonTap: selectFiat,
           // isWalletCoin: isWalletCoin(coin, true),
           isWalletCoin: false,
           // image: _fetchIconUrlFromTicker(ref
