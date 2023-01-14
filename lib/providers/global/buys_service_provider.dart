79edf1292d968e52f087f2248590285284f14187
sneurlax <sneurlax@gmail.com>
2023-01-13 19:12:03 -0600

    remove unneeded buy type and buys service
    no history needed at this time

--- lib/providers/global/buys_service_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:stackwallet/services/buys_service.dart';
-
-final buysServiceProvider =
-    ChangeNotifierProvider<BuysService>((ref) => BuysService());
