12ba1fa24a71fe38f83348a2042e29ef30b88277
sneurlax <sneurlax@gmail.com>
2023-01-23 15:02:30 -0600

    add OLED Black and Fruit Sorbet theme cases to currency text style

--- lib/utilities/text_styles.dart
+++ lib/utilities/text_styles.dart
@@ -1393,6 +1393,30 @@ class STextStyles {
             ..strokeCap = StrokeCap.round
             ..style = PaintingStyle.stroke,
         );
+      // case ThemeType.OLEDblack:
+      //   return GoogleFonts.inter(
+      //     color: _theme(context).textDark,
+      //     fontWeight: FontWeight.w500,
+      //     fontSize: 16,
+      //     background: Paint()
+      //       ..color = Theme.of(context).extension<StackColors>()!.highlight
+      //       ..strokeWidth = 8
+      //       ..strokeJoin = StrokeJoin.round
+      //       ..strokeCap = StrokeCap.round
+      //       ..style = PaintingStyle.stroke,
+      //   );
+      // case ThemeType.fruitSorbet:
+      //   return GoogleFonts.inter(
+      //     color: _theme(context).textDark,
+      //     fontWeight: FontWeight.w500,
+      //     fontSize: 16,
+      //     background: Paint()
+      //       ..color = Theme.of(context).extension<StackColors>()!.highlight
+      //       ..strokeWidth = 8
+      //       ..strokeJoin = StrokeJoin.round
+      //       ..strokeCap = StrokeCap.round
+      //       ..style = PaintingStyle.stroke,
+      //   );
     }
   }
 }
