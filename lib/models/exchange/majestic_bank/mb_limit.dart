8bdf0413bd1b707d2cc966ca966fa7b76a7353e3
julian <julian@cypherstack.com>
2023-02-01 18:24:41 -0600

    majestic bank limits and rates api calls

---
+++ lib/models/exchange/majestic_bank/mb_limit.dart
@@ -0,0 +1,19 @@
+import 'package:decimal/decimal.dart';
+import 'package:stackwallet/models/exchange/majestic_bank/mb_object.dart';
+
+class MBLimit extends MBObject {
+  MBLimit({
+    required this.currency,
+    required this.min,
+    required this.max,
+  });
+
+  final String currency;
+  final Decimal min;
+  final Decimal max;
+
+  @override
+  String toString() {
+    return "MBLimit: { $currency: { min: $min, max: $max } }";
+  }
+}
