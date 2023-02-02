eaf8fe7094a77defa21c15932b608c843b371259
julian <julian@cypherstack.com>
2023-02-02 08:48:03 -0600

    possible epic cash address get fix

--- lib/services/coins/epiccash/epiccash_wallet.dart
+++ lib/services/coins/epiccash/epiccash_wallet.dart
@@ -839,6 +839,10 @@ class EpicCashWallet extends CoinServiceAPI
     isar_models.Address? address = await db
         .getAddresses(walletId)
         .filter()
+        .subTypeEqualTo(isar_models.AddressSubType.receiving)
+        .and()
+        .typeEqualTo(isar_models.AddressType.mimbleWimble)
+        .and()
         .derivationIndexEqualTo(index)
         .findFirst();
 
@@ -877,8 +881,14 @@ class EpicCashWallet extends CoinServiceAPI
       (await _currentReceivingAddress)?.value ??
       (await _getReceivingAddressForIndex(0)).value;
 
-  Future<isar_models.Address?> get _currentReceivingAddress =>
-      db.getAddresses(walletId).sortByDerivationIndexDesc().findFirst();
+  Future<isar_models.Address?> get _currentReceivingAddress => db
+      .getAddresses(walletId)
+      .filter()
+      .subTypeEqualTo(isar_models.AddressSubType.receiving)
+      .and()
+      .typeEqualTo(isar_models.AddressType.mimbleWimble)
+      .sortByDerivationIndexDesc()
+      .findFirst();
 
   @override
   Future<void> exit() async {
