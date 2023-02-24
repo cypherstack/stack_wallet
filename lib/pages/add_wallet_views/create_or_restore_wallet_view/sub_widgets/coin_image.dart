9a9f71ea3e10518cb0251194d73c6b4bf2e1fbc0
ryleedavis <rylee@cypherstack.com>
2023-02-24 15:11:04 -0700

    fix coin image <def> issue

--- lib/pages/add_wallet_views/create_or_restore_wallet_view/sub_widgets/coin_image.dart
+++ lib/pages/add_wallet_views/create_or_restore_wallet_view/sub_widgets/coin_image.dart
@@ -27,7 +27,7 @@ class CoinImage extends ConsumerWidget {
                 coin != Coin.epicCash &&
                 coin != Coin.monero &&
                 coin != Coin.namecoin) ||
-            isForest)
+            (isForest && coin != Coin.dogecoin))
         ? SvgPicture.asset(
             Assets.svg.imageFor(coin: coin, context: context),
             width: isDesktop ? 324 : MediaQuery.of(context).size.width,
