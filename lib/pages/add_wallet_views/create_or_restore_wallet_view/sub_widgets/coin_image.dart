a7b88647bc35288bda42ddce2bf7026a65bb2090
ryleedavis <rylee@cypherstack.com>
2023-02-24 09:43:50 -0700

    fix svg def issue + changed forest coin images

--- lib/pages/add_wallet_views/create_or_restore_wallet_view/sub_widgets/coin_image.dart
+++ lib/pages/add_wallet_views/create_or_restore_wallet_view/sub_widgets/coin_image.dart
@@ -23,19 +23,19 @@ class CoinImage extends ConsumerWidget {
     final bool isForest =
         ref.read(colorThemeProvider.state).state.themeType == ThemeType.forest;
 
-    return ((isSorbet || isForest) &&
-            coin != Coin.epicCash &&
-            coin != Coin.monero &&
-            coin != Coin.namecoin)
-        ? ((isForest && coin != Coin.litecoin && coin != Coin.dogecoin)
-            ? SvgPicture.asset(
-                Assets.svg.imageFor(coin: coin, context: context),
-                width: isDesktop ? 324 : MediaQuery.of(context).size.width,
-              )
-            : Image(
-                image: AssetImage(
-                Assets.png.imageFor(coin: coin, context: context),
-              )))
+    return ((isSorbet &&
+                coin != Coin.epicCash &&
+                coin != Coin.monero &&
+                coin != Coin.namecoin) ||
+            isForest)
+        ? SvgPicture.asset(
+            Assets.svg.imageFor(coin: coin, context: context),
+            width: isDesktop ? 324 : MediaQuery.of(context).size.width,
+          )
+        // : Image(
+        //     image: AssetImage(
+        //     Assets.png.imageFor(coin: coin, context: context),
+        //   )))
         : Image(
             image: AssetImage(
               Assets.png.imageFor(coin: coin, context: context),
