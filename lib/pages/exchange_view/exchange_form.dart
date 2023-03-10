a5a2dcef64766a86f80bc1eb73ac7353d2fa833b
julian <julian@cypherstack.com>
2023-03-10 16:47:48 -0600

    set wallet coin in exchange view

--- lib/pages/exchange_view/exchange_form.dart
+++ lib/pages/exchange_view/exchange_form.dart
@@ -618,6 +618,16 @@ class _ExchangeFormState extends ConsumerState<ExchangeForm> {
     if (walletInitiated) {
       WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
         ref.read(exchangeFormStateProvider).reset(shouldNotifyListeners: true);
+        ExchangeDataLoadingService.instance
+            .getAggregateCurrency(
+          coin!.ticker,
+          ExchangeRateType.estimated,
+        )
+            .then((value) {
+          if (value != null) {
+            ref.read(exchangeFormStateProvider).updateSendCurrency(value, true);
+          }
+        });
       });
     } else {
       _sendController.text =
