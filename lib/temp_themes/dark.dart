38363a65d7dd5312cf40c86cf5b86bfc41f93931
Likho <likhojiba@gmail.com>
2023-04-19 14:22:01 +0200

    Dark theme json

--- lib/temp_themes/dark.dart
+++ lib/temp_themes/dark.dart
@@ -1,1 +1,212 @@
-final Map<String, dynamic> darkJson = {};
+final Map<String, dynamic> darkJson = {
+  "name": "Dark",
+  "assetBundleUrl": "",
+  "brightness": "", //TO - Brightness
+  "icon_colors": {
+    "bitcoin": "0xFFFCC17B",
+    "litecoin": "0xFF7FA6E1",
+    "bitcoincash": "0xFF7BCFB8",
+    "firo": "0xFFFF897A",
+    "dogecoin": "0xFFFFE079",
+    "epicCash": "0xFFC5C7CB",
+    "ethereum": "0xFFA7ADE9",
+    "monero": "0xFFFF9E6B",
+    "namecoin": "0xFF91B1E1",
+    "wownero": "0xFFED80C1",
+    "particl": "0xFF8175BD"
+  },
+  "background_colors": {
+    "background": "0xFF2A2D34",
+    "backgroundAppBar": "0xFF2A2D34overlay"
+  },
+  "gradients": {
+    "background": {
+      "type": "Linear",
+      "begin": {"x": 0, "y": 1},
+      "end": {"x": -1, "y": 1},
+      "colors": ["0xFF638227", "0xFF638227"]
+    }
+  },
+  "overlay": "0xFF111215",
+  "accent_colors": {
+    "blue": "0xFF4C86E9",
+    "green": "0xFF4CC0A0",
+    "yellow": "0xFFF7D65D",
+    "red": "0xFFD34E50",
+    "orange": "0xFFFEA68D",
+    "dark": "0xFFF3F3F3"
+  },
+  "shadow": "0x0F2D3132",
+  "text_colors": {
+    "dark_one": "0xFFF3F3F3",
+    "dark_two": "0xFFDBDBDB",
+    "dark_three": "0xFFEEEFF1",
+    "white": "0xFF232323",
+    "favorite": "0xFF232323",
+    "error": "0xFFF37475",
+    "restore": "0xFF111215"
+  },
+  "text_subtitles": {
+    "subtitle_one": "0xFF9E9E9E",
+    "subtitle_two": "0xFF969696",
+    "subtitle_three": "0xFFA9ACAC",
+    "subtitle_four": "0xFF8E9192",
+    "subtitle_five": "0xFF747778",
+    "subtitle_six": "0xFF414141"
+  },
+  "button_text_colors": {
+    "primary": "0xFFFFFFFF",
+    "secondary": "0xFFFFFFFF",
+    "primary_disabled": "0xFFFFFFFF",
+    "secondary_disabled": "0xFF6A6C71",
+    "border": "0xFF4C86E9",
+    "disabled": "0xFF314265",
+    "borderless": "0xFF4C86E9",
+    "borderless_disabled": "0xFFB6B6B6"
+  },
+  "number_text_default": "0xFFFFFFFF",
+  "numpad_text_default": "0xFFFFFFFF",
+  "bottom_nav_text": "0xFFFFFFFF",
+  "custom_text_button_enabled_text": "0xFF4C86E9",
+  "custom_text_button_disabled_text": "0xFF9E9E9E",
+  "switch_colors": {
+    "bg_on": "0xFF4C86E9",
+    "bg_off": "0xFFC1D9FF",
+    "bg_disabled": "0xFFB5B7BA",
+    "circle_on": "0xFFC9DDFF",
+    "circle_off": "0xFFFFFFFF",
+    "circle_disabled": "0xFFFFFFFF"
+  },
+  "step_indicator": {
+    "bg_check": "0xFF4C86E9",
+    "bg_number": "0xFF4C86E9",
+    "bg_inactive": "0xFF3B3F46",
+    "bg_lines": "0xFF4C86E9",
+    "bg_lines_inactive": "0xFF3B3F46",
+    "icon_text": "0xFFFFFFFF",
+    "icon_number": "0xFFFFFFFF",
+    "icon_inactive": "0xFF747474"
+  },
+  "checkbox": {
+    "bg_checked": "0xFF4C86E9",
+    "border_empty": "0xFF8E9192",
+    "bg_disabled": "0xFFADC7EC",
+    "icon_checked": "0xFFFFFFFF",
+    "icon_disabled": "0xFFFFFFFF",
+    "text_label": "0xFFFFFFFF"
+  },
+  "snack_bar": {
+    "back_success": "0xFF8EF5C3",
+    "back_error": "0xFFFFB4A9",
+    "back_info": "0xFFB4C4FF",
+    "text_success": "0xFF003921",
+    "text_error": "0xFF690001",
+    "text_info": "0xFF00297A"
+  },
+  "icons": {
+    "bottom_nav_icon_back": "0xFF7F8185",
+    "bottom_nav_icon_icon": "0xFFFFFFFF",
+    "top_nav_icon_primary": "0xFFFFFFFF",
+    "top_nav_icon_green": "0xFF4CC0A0",
+    "top_nav_icon_yellow": "0xFFF7D65D",
+    "top_nav_icon_red": "0xFFD34E50",
+    "settings_icon_back": "0xFFE0E3E3",
+    "settings_icon_icon": "0xFF232323",
+    "settings_icon_back_two": "0xFF94D6C4",
+    "settings_icon_element": "0xFF00A578"
+  },
+  "text_field": {
+    "active_bg": "0xFF4C5360",
+    "default_bg": "0xFF444953",
+    "error_bg": "0xFFFFB4A9",
+    "success_bg": "0xFF8EF5C3",
+    "error_border": "0xFFFFB4A9",
+    "success_border": "0xFF8EF5C3",
+    "active_search_icon_left": "0xFFA9ACAC",
+    "default_search_icon_left": "0xFFA9ACAC",
+    "error_search_icon_left": "0xFF690001",
+    "success_search_icon_left": "0xFF003921",
+    "active_text": "0xFFFFFFFF",
+    "default_text": "0xFFA9ACAC",
+    "error_text": "0xFF000000",
+    "success_text": "0xFF000000",
+    "active_label": "0xFFA9ACAC",
+    "error_label": "0xFF690001",
+    "success_label": "0xFF003921",
+    "active_search_icon_right": "0xFFC4C7C7",
+    "default_search_icon_right": "0xFF747778",
+    "error_search_icon_right": "0xFF690001",
+    "success_search_icon_right": "0xFF003921"
+  },
+  "settings_item_level_two": {
+    "active_bg": "0xFF484B51",
+    "active_text": "0xFFFFFFFF",
+    "active_sub": "0xFF9E9E9E"
+  },
+  "radio_button": {
+    "icon_border": "0xFF4C86E9",
+    "icon_border_disabled": "0xFF9E9E9E",
+    "border_enabled": "0xFF4C86E9",
+    "border_disabled": "0xFFCDCDCD",
+    "icon_circle": "0xFF9E9E9E",
+    "icon_enabled": "0xFF4C86E9",
+    "text_enabled": "0xFF44464E",
+    "text_disabled": "0xFF44464E",
+    "label_enabled": "0xFF8E9192",
+    "label_disabled": "0xFF8E9192"
+  },
+  "info_text": {
+    "item_bg": "0xFF333942",
+    "item_label": "0xFF9E9E9E",
+    "item_text": "0xFFFFFFFF",
+    "item_icons": "0xFF4C86E9"
+  },
+  "popup_bg": "0xFF333942",
+  "currency_list_item_bg": "0xFF484B51",
+  "bottom_nav": {
+    "sw_bg": "0xFF35383D",
+    "sw_mid": "0xFF292D34",
+    "sw_bottom": "0xFFFFFFFF",
+    "shadow": "0xFF282E33",
+    "splash": "0x358E9192",
+    "highlight": "0x44A9ACAC",
+    "warning_foreground": "0xFF690001",
+    "warning_background": "0xFFFFB4A9",
+    "loading_overlay_text_color": "0xFFF7F7F7",
+    "my_stack_contact_icon_bg": "0x88747778",
+    "text_confirm_total_amount": "0xFF003921",
+    "text_selected_word_table_iterm": "0xFF00297A"
+  },
+  "favorite_star": {"active": "0xFFF7D65D", "inactive": "0xFF969696"},
+  "rate_type_toggle": {
+    "color_on": "0xFF444953",
+    "color_off": "0xFF333942",
+    "desktop_color_on": "0xFF444953",
+    "desktop_color_off": "0xFF444E5C"
+  },
+  "token_view": {
+    "eth_tag_text": "0xFFFFFFFF",
+    "eth_tag_bg": "0xFF5761A2",
+    "eth_wallet_tag_text": "0xFFE7EBFF",
+    "eth_wallet_tag_bg": "0xFF414868",
+    "token_summary_text_primary": "0xFFFFFFFF",
+    "token_summary_text_secondary": "0xFFC9D0FF",
+    "token_summary_bg": "0xFF464C73",
+    "token_summary_button_bg": "0xFFC9D0FF",
+    "token_summary_icon": "0xFF252C78"
+  },
+  "box_shadows": [
+    {
+      "type": "standard",
+      "color": "0x0F2D3132",
+      "spread_radius": 3,
+      "blur_radius": 4
+    },
+    {
+      "type": "home_view_button_bar",
+      "color": "0x0F2D3132",
+      "spread_radius": 3,
+      "blur_radius": 4
+    }
+  ]
+};
