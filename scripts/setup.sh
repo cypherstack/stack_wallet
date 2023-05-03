8790bdcc0c423a06e02b1f1ca57e3a0efb6bbcf1
Josh Babb <sneurlax@gmail.com>
2023-05-03 16:43:40 -0500

    add packages from manual build docs to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -25,21 +25,24 @@ cd stack_wallet
 export STACK_WALLET=$(pwd)
 git submodule update --init --recursive
 
-# Create template lib/external_api_keys.dart file if it doesn't already exist
+# create template lib/external_api_keys.dart file if it doesn't already exist
 KEYS="$HOME/projects/stack_wallet/lib/external_api_keys.dart"
 if ! test -f "$KEYS"; then
     echo 'prebuild.sh: creating template lib/external_api_keys.dart file'
     printf 'const kChangeNowApiKey = "";\nconst kSimpleSwapApiKey = "";' > $KEYS
 fi
 
-#install stack wallet dependencies
+# install stack wallet dependencies
 sudo apt-get install -y unzip automake build-essential file pkg-config git python libtool libtinfo5 cmake openjdk-8-jre-headless libgit2-dev clang libncurses5-dev libncursesw5-dev zlib1g-dev llvm 
 
 sudo apt-get install -y debhelper libclang-dev cargo rustc opencl-headers libssl-dev ocl-icd-opencl-dev
 
-sudo apt-get install -y unzip automake build-essential file pkg-config git python libtool libtinfo5 cmake openjdk-8-jre-headless libsecret-1-dev
+sudo apt-get install -y unzip automake build-essential file pkg-config git python libtool libtinfo5 cmake openjdk-8-jre-headless
 
 sudo apt install -y libc6-dev-i386
+
+# linux desktop dependencies
+sudo apt-get install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev meson python3-pip libgirepository1.0-dev valac xsltproc docbook-xsl
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
 source "$HOME/.cargo/env"
