4f97f8fbb403e5c947b468608af08aad31546ef7
Josh Babb <sneurlax@gmail.com>
2023-05-11 16:47:02 -0500

    update linux and android build_all scripts to also run tor's scripts TODO windows

--- scripts/android/build_all.sh
+++ scripts/android/build_all.sh
@@ -7,6 +7,7 @@ mkdir build
 (cd ../../crypto_plugins/flutter_liblelantus/scripts/android && ./build_all.sh ) &
 (cd ../../crypto_plugins/flutter_libepiccash/scripts/android && ./install_ndk.sh && ./build_all.sh )  &
 (cd ../../crypto_plugins/flutter_libmonero/scripts/android/ && ./build_all.sh  ) &
+(cd ../../crypto_plugins/tor/scripts/android && ./install_ndk.sh && ./build_all.sh )  &
 
 wait
 echo "Done building"
