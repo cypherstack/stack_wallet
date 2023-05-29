838e623ff2ba96581f9ff85086d2717d94e8befd
julian <julian@cypherstack.com>
2023-05-29 10:39:14 -0600

    fix: update fiat price on fiat currency selection changed

--- lib/pages/settings_views/global_settings_view/currency_view.dart
+++ lib/pages/settings_views/global_settings_view/currency_view.dart
@@ -50,6 +50,10 @@ class _CurrencyViewState extends ConsumerState<BaseCurrencySettingsView> {
       currenciesWithoutSelected.remove(current);
       currenciesWithoutSelected.insert(0, current);
       ref.read(prefsChangeNotifierProvider).currency = current;
+
+      if (ref.read(prefsChangeNotifierProvider).externalCalls) {
+        ref.read(priceAnd24hChangeNotifierProvider).updatePrice();
+      }
     }
   }
 
