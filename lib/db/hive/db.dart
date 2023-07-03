fe8ad00b9b32c9e6195a9b490afa379ead0501bc
julian <julian@cypherstack.com>
2023-07-03 16:42:45 -0600

    don't open legacy trades box unless during migration

--- lib/db/hive/db.dart
+++ lib/db/hive/db.dart
@@ -13,7 +13,6 @@ import 'dart:isolate';
 import 'package:cw_core/wallet_info.dart' as xmr;
 import 'package:hive/hive.dart';
 import 'package:mutex/mutex.dart';
-import 'package:stackwallet/models/exchange/change_now/exchange_transaction.dart';
 import 'package:stackwallet/models/exchange/response_objects/trade.dart';
 import 'package:stackwallet/models/node_model.dart';
 import 'package:stackwallet/models/notification_model.dart';
@@ -62,7 +61,6 @@ class DB {
   Box<NotificationModel>? _boxNotifications;
   Box<NotificationModel>? _boxWatchedTransactions;
   Box<NotificationModel>? _boxWatchedTrades;
-  Box<ExchangeTransaction>? _boxTrades;
   Box<Trade>? _boxTradesV2;
   Box<String>? _boxTradeNotes;
   Box<String>? _boxFavoriteWallets;
@@ -137,7 +135,6 @@ class DB {
         await Hive.openBox<NotificationModel>(boxNameWatchedTransactions);
     _boxWatchedTrades =
         await Hive.openBox<NotificationModel>(boxNameWatchedTrades);
-    _boxTrades = await Hive.openBox<ExchangeTransaction>(boxNameTrades);
     _boxTradesV2 = await Hive.openBox<Trade>(boxNameTradesV2);
     _boxTradeNotes = await Hive.openBox<String>(boxNameTradeNotes);
     _boxTradeLookup = await Hive.openBox<TradeWalletLookup>(boxNameTradeLookup);
