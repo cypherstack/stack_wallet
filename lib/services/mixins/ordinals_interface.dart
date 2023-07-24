ce1bd8212fc4326ad580420475a9b27e81d4e14c
sneurlax <sneurlax@gmail.com>
2023-07-23 23:18:23 -0500

    remove Future<bool> inscriptionInAddresses(List<String> addresses)
    inscriptionInAddress is next, after its usage is removed from _updateUTXOs, updateUTXOs (or another hook for detecting if a refresh has detected a new output) will trigger the more general refreshInscriptions

--- lib/services/mixins/ordinals_interface.dart
+++ lib/services/mixins/ordinals_interface.dart
@@ -91,14 +91,4 @@ mixin OrdinalsInterface {
       return false;
     }
   }
-
-  // check if an inscription is in a given <UTXO> output
-  Future<bool> inscriptionInAddresses(List<String> addresses) async {
-    var inscriptions = await _getInscriptionDataFromAddresses(addresses);
-    if (inscriptions.isNotEmpty) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 }
