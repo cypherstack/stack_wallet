c584cb862dd445944ca055eaa0230fe00080ab29
sneurlax <sneurlax@gmail.com>
2023-07-27 14:47:16 -0500

    don't assume list isn't null

--- lib/services/litescribe_api.dart
+++ lib/services/litescribe_api.dart
@@ -44,7 +44,10 @@ class LitescribeAPI {
     // Check if the number of returned inscriptions equals the limit
     final list = response.data['result']['list'];
     final int total = response.data['result']['total'] as int;
-    final int currentSize = list.length as int;
+    int currentSize = 0;
+    if (list != null) {
+      currentSize = list.length as int;
+    }
 
     if (currentSize == size && currentSize < total) {
       // If the number of returned inscriptions equals the limit and there are more inscriptions available,
