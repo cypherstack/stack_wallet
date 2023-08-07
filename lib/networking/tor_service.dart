53b8adca16cdfa092dc76df539d56a5eb8100f02
julian <julian@cypherstack.com>
2023-08-07 10:39:04 -0600

    basic tor service

---
+++ lib/networking/tor_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stackwallet/utilities/stack_file_system.dart';
+import 'package:tor/tor.dart';
+
+final pTorService = Provider((_) => TorService());
+
+class TorService {
+  final _tor = Tor();
+
+  int get port => _tor.port;
+
+  Future<void> start() async {
+    final dir = await StackFileSystem.applicationTorDirectory();
+    return await _tor.start(torDir: dir);
+  }
+
+  Future<void> stop() async {
+    return await _tor.disable();
+  }
+}
