1781774d45000bfa46ac6a1f7a2cbb63c71134fd
julian <julian@cypherstack.com>
2023-09-20 10:32:07 -0600

    script error wording

--- scripts/rust_version.sh
+++ scripts/rust_version.sh
@@ -4,7 +4,7 @@ set_rust_to_1680() {
   if rustup toolchain list | grep -q "1.68.0"; then
     rustup default 1.68.0
   else
-    echo "Rust version 1.68.0 is not installed. Please install it using 'rustup toolchain install 1.68.0'." >&2
+    echo "Rust version 1.68.0 is not installed. Please install it using 'rustup install 1.68.0'." >&2
     exit 1
   fi
 }
@@ -13,7 +13,7 @@ set_rust_to_1720() {
   if rustup toolchain list | grep -q "1.72.0"; then
     rustup default 1.72.0
   else
-    echo "Rust version 1.72.0 is not installed. Please install it using 'rustup toolchain install 1.72.0'." >&2
+    echo "Rust version 1.72.0 is not installed. Please install it using 'rustup install 1.72.0'." >&2
     exit 1
   fi
 }
