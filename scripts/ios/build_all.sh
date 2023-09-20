09c6d440c8d93eb557288db7a61ee1ada19c5fd1
julian <julian@cypherstack.com>
2023-09-20 16:56:06 -0600

    ios rust targets updates/checks

--- scripts/ios/build_all.sh
+++ scripts/ios/build_all.sh
@@ -6,6 +6,10 @@ set -e
 source ../rust_version.sh
 set_rust_to_1680
 
+# ensure ios rust triples are there
+rustup target add aarch64-apple-ios
+rustup target add x86_64-apple-ios
+
 (cd ../../crypto_plugins/flutter_liblelantus/scripts/ios && ./build_all.sh ) &
 (cd ../../crypto_plugins/flutter_libepiccash/scripts/ios && ./build_all.sh )  &
 (cd ../../crypto_plugins/flutter_libmonero/scripts/ios/ && ./build_all.sh  ) &
@@ -15,3 +19,7 @@ echo "Done building"
 
 # set rust (back) to a more recent stable release to allow stack wallet to build tor
 set_rust_to_1720
+
+# ensure ios rust triples are there
+rustup target add aarch64-apple-ios
+rustup target add x86_64-apple-ios
