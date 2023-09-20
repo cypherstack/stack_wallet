cd00e9f8fd6bfe085dbe85d920ca6eca61230751
sneurlax <sneurlax@gmail.com>
2023-09-20 13:39:45 -0500

    build monero by default
    even if it isn't used

--- scripts/windows/build_all.sh
+++ scripts/windows/build_all.sh
@@ -9,7 +9,7 @@ set_rust_to_1680
 mkdir -p build
 (cd ../../crypto_plugins/flutter_libepiccash/scripts/windows && ./build_all.sh )  &
 (cd ../../crypto_plugins/flutter_liblelantus/scripts/windows && ./build_all.sh ) &
-# (cd ../../crypto_plugins/flutter_libmonero/scripts/windows && ./build_all.sh) &
+(cd ../../crypto_plugins/flutter_libmonero/scripts/windows && ./build_all.sh) &
 
 wait
 echo "Done building"
