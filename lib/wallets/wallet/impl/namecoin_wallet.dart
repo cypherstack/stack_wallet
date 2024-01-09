ef15382c13e7c8ea8ecef3ca8bf478efcb5dbcc1
sneurlax <sneurlax@gmail.com>
2024-01-09 17:25:33 -0600

    typofix

--- lib/wallets/wallet/impl/namecoin_wallet.dart
+++ lib/wallets/wallet/impl/namecoin_wallet.dart
@@ -72,7 +72,7 @@ class NamecoinWallet extends Bip39HDWallet
   Future<void> updateTransactions() async {
     final currentChainHeight = await fetchChainHeight();
 
-    // TODO: [prio=a] switch to V2 transactions
+    // TODO: [prio=low] switch to V2 transactions.
     final data = await fetchTransactionsV1(
       addresses: await fetchAddressesForElectrumXScan(),
       currentChainHeight: currentChainHeight,
