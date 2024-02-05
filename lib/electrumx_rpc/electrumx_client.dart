3ec6e2a00819320a6c22c7daaf1f27e852ba7b70
sneurlax <sneurlax@gmail.com>
2024-02-05 15:23:08 -0600

    TODO notes

--- lib/electrumx_rpc/electrumx_client.dart
+++ lib/electrumx_rpc/electrumx_client.dart
@@ -202,6 +202,7 @@ class ElectrumXClient {
           // ... But if the killswitch is set, then we throw an exception.
           throw Exception(
               "Tor preference and killswitch set but Tor is not enabled, not connecting to ElectrumX");
+          // TODO [prio=low]: Restart Tor. Update Tor package for restart feature.
         }
       } else {
         // Get the proxy info from the TorService.
