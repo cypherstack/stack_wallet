515f12da000e103123a00cabf25257c23144f528
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-03-22 05:48:21 -0400

    fix: crash in monero_c 25 words seed (didn't test yet)

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -187,7 +187,7 @@ abstract class Constants {
 
       case Coin.monero:
       case Coin.monerodart:
-        values.addAll([16]);
+        values.addAll([16, 25]);
         break;
       case Coin.wownero:
         values.addAll([14, 25]);
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -18,6 +18,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
   coinlib_flutter
   flutter_libsparkmobile
+  monero_libs
   tor_ffi_plugin
 )
 
