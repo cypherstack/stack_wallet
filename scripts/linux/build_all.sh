08f7992e51bdaee9bbd9d1a7c29bfa25ef3f569e
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-03-30 07:35:36 +0100

    make scripts use less CPU

--- scripts/linux/build_all.sh
+++ scripts/linux/build_all.sh
@@ -12,9 +12,9 @@ set_rust_to_1671
 # flutter-elinux build linux --dart-define="IS_ARM=true"
 mkdir -p build
 ./build_secure_storage_deps.sh &
-(cd ../../crypto_plugins/flutter_liblelantus/scripts/linux && ./build_all.sh ) &
-(cd ../../crypto_plugins/flutter_libepiccash/scripts/linux && ./build_all.sh )  &
-(cd ../../crypto_plugins/flutter_libmonero/scripts/linux && ./build_monero_all.sh && ./build_sharedfile.sh ) &
+(cd ../../crypto_plugins/flutter_liblelantus/scripts/linux && ./build_all.sh )
+(cd ../../crypto_plugins/flutter_libepiccash/scripts/linux && ./build_all.sh )
+(cd ../../crypto_plugins/flutter_libmonero/scripts/linux && ./build_monero_all.sh && ./build_sharedfile.sh )
 
 wait
 echo "Done building"
