5e99b2f1e39dc2602fe83725d8912974517b0e95
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-03-30 07:35:36 +0100

    make scripts use less CPU

--- scripts/android/build_all.sh
+++ scripts/android/build_all.sh
@@ -10,11 +10,13 @@ mkdir -p build
 . ./config.sh
 ./install_ndk.sh
 
-(cd ../../crypto_plugins/flutter_liblelantus/scripts/android && ./build_all.sh ) &
-(cd ../../crypto_plugins/flutter_libepiccash/scripts/android && ./install_ndk.sh && ./build_opensll.sh && ./build_all.sh )  &
-(cd ../../crypto_plugins/flutter_libmonero/scripts/android/ && ./build_all.sh  ) &&
-set_rust_to_1720 &
-(cd ../../crypto_plugins/frostdart/scripts/android && ./build_all.sh ) &
+(cd ../../crypto_plugins/flutter_liblelantus/scripts/android && ./build_all.sh )
+(cd ../../crypto_plugins/flutter_libepiccash/scripts/android && ./install_ndk.sh && ./build_openssl.sh && ./build_all.sh )
+(cd ../../crypto_plugins/flutter_libmonero/scripts/android/ && ./build_all.sh)
+
+wait
+set_rust_to_1720
+(cd ../../crypto_plugins/frostdart/scripts/android && ./build_all.sh )
 
 wait
 echo "Done building"
