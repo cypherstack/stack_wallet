6189f2f9c567d20045870545e557f2e0fd0865e6
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-04-03 22:34:50 +0200

    added monero_c to build_all.sh for macos and ios

--- scripts/ios/build_all.sh
+++ scripts/ios/build_all.sh
@@ -14,11 +14,22 @@ rustup target add x86_64-apple-ios
 rustup target add aarch64-apple-ios
 rustup target add x86_64-apple-ios
 
-(cd ../../crypto_plugins/flutter_liblelantus/scripts/ios && ./build_all.sh ) &
-(cd ../../crypto_plugins/flutter_libepiccash/scripts/ios && ./build_all.sh )  &
-(cd ../../crypto_plugins/flutter_libmonero/scripts/ios/ && ./build_all.sh  ) &
+(cd ../../crypto_plugins/flutter_liblelantus/scripts/ios && ./build_all.sh )
+(cd ../../crypto_plugins/flutter_libepiccash/scripts/ios && ./build_all.sh )
+(cd ../../crypto_plugins/flutter_libmonero/scripts/ios   && ./build_all.sh )
 
 wait
+
+pushd ../../crypto_plugins/monero_c
+    ./apply_patches.sh monero
+    ./apply_patches.sh wownero
+    rm -rf external/ios/build/ios/
+    ./build_single.sh monero host-apple-ios -j8
+    ./build_single.sh wownero host-apple-ios -j8
+
+    unxz -f release/*/*.xz
+popd
+
 echo "Done building"
 
 # set rust (back) to a more recent stable release to allow stack wallet to build tor
--- scripts/macos/build_all.sh
+++ scripts/macos/build_all.sh
@@ -11,6 +11,16 @@ set_rust_to_1671
 (cd ../../crypto_plugins/flutter_libmonero/scripts/macos/ && ./build_all.sh  )
 
 wait
+
+pushd ../../crypto_plugins/monero_c
+    ./apply_patches.sh monero
+    ./apply_patches.sh wownero
+    ./build_single.sh monero host-apple-darwin -j8
+    ./build_single.sh wownero host-apple-darwin -j8
+
+    unxz -f release/*/*.xz
+popd
+
 echo "Done building"
 
 # set rust (back) to a more recent stable release to allow stack wallet to build tor
