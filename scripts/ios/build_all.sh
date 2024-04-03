6876c9ad8ccc4a807814c2ce01d7634eb18da726
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-04-03 22:34:50 +0200

    added monero_c to build_all.sh for macos and ios

--- scripts/ios/build_all.sh
+++ scripts/ios/build_all.sh
@@ -14,13 +14,25 @@ rustup target add x86_64-apple-ios
 rustup target add aarch64-apple-ios
 rustup target add x86_64-apple-ios
 
-(cd ../../crypto_plugins/flutter_liblelantus/scripts/ios && ./build_all.sh ) &
-(cd ../../crypto_plugins/flutter_libepiccash/scripts/ios && ./build_all.sh )  &
-(cd ../../crypto_plugins/flutter_libmonero/scripts/ios/ && ./build_all.sh  ) &
-set_rust_to_1720 &
-(cd ../../crypto_plugins/frostdart/scripts/ios && ./build_all.sh ) &
+(cd ../../crypto_plugins/flutter_liblelantus/scripts/ios && ./build_all.sh )
+(cd ../../crypto_plugins/flutter_libepiccash/scripts/ios && ./build_all.sh )
+(cd ../../crypto_plugins/flutter_libmonero/scripts/ios   && ./build_all.sh )
 
 wait
+set_rust_to_1720
+(cd ../../crypto_plugins/frostdart/scripts/ios && ./build_all.sh )
+
+wait
+pushd ../../crypto_plugins/monero_c
+    ./apply_patches.sh monero
+    ./apply_patches.sh wownero
+    rm -rf external/ios/build/ios/
+    ./build_single.sh monero host-apple-ios -j8
+    ./build_single.sh wownero host-apple-ios -j8
+
+    unxz -f release/*/*.xz
+popd
+
 echo "Done building"
 
 # ensure ios rust triples are there
