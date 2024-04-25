627e3e68df8c94f91da72e6327b2636ffadbb75e
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-04-26 00:44:11 +0200

    fix build issues

--- scripts/android/build_all.sh
+++ scripts/android/build_all.sh
@@ -10,9 +10,9 @@ mkdir -p build
 . ./config.sh
 ./install_ndk.sh
 
-(cd ../../crypto_plugins/flutter_liblelantus/scripts/android && ./build_all.sh ) &
-(cd ../../crypto_plugins/flutter_libepiccash/scripts/android && ./install_ndk.sh && ./build_opensll.sh && ./build_all.sh )  &
-(cd ../../crypto_plugins/flutter_libmonero/scripts/android/ && ./build_all.sh  ) &
+(cd ../../crypto_plugins/flutter_liblelantus/scripts/android && ./build_all.sh )
+(cd ../../crypto_plugins/flutter_libepiccash/scripts/android && ./install_ndk.sh && ./build_openssl.sh && ./build_all.sh )
+(cd ../../crypto_plugins/flutter_libmonero/scripts/android/ && ./build_all.sh )
 
 wait
 echo "Done building"
