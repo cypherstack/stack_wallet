fb0b87942a8c3da4a3ad8ea168237f6920814761
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-04-26 14:21:21 +0200

    wownero support

--- lib/pages/add_wallet_views/add_wallet_view/add_wallet_view.dart
+++ lib/pages/add_wallet_views/add_wallet_view/add_wallet_view.dart
@@ -131,7 +131,7 @@ class _AddWalletViewState extends ConsumerState<AddWalletView> {
       _coins.remove(Coin.monero);
       _coins.remove(Coin.wownero);
     } else if (Platform.isLinux) {
-      _coins.remove(Coin.wownero);
+      // _coins.remove(Coin.wownero);
     }
 
     coinEntities.addAll(_coins.map((e) => CoinEntity(e)));
