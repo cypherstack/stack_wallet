cddfa6b37e53b890fde2c8798bb3242238b6727a
sneurlax <sneurlax@gmail.com>
2024-05-15 17:51:21 -0500

    build_all_duo.sh scripts

---
+++ scripts/android/build_all_duo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+./configure_duo.sh
+cd ..
+./configure_duo.sh
+cd android
+
+# libepiccash requires old rust
+source ../rust_version.sh
+set_rust_to_1671
+
+mkdir -p build
+. ./config.sh
+./install_ndk.sh
+
+PLUGINS_DIR=../../crypto_plugins
+
+(cd "${PLUGINS_DIR}"/flutter_liblelantus/scripts/android && ./build_all.sh ) &
+(cd "${PLUGINS_DIR}"/flutter_libepiccash/scripts/android && ./build_all.sh )  &
+(cd "${PLUGINS_DIR}"/flutter_libmonero/scripts/android/ && ./build_all.sh  ) &&
+set_rust_to_1720 &&
+(cd "${PLUGINS_DIR}"/frostdart/scripts/android && ./build_all.sh ) &
+
+wait
+echo "Done building"
---
+++ scripts/ios/build_all_duo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+./configure_duo.sh
+cd ..
+./configure_duo.sh
+cd ios
+
+# libepiccash requires old rust
+source ../rust_version.sh
+set_rust_to_1671
+
+# ensure ios rust triples are there
+rustup target add aarch64-apple-ios
+rustup target add x86_64-apple-ios
+
+# ensure ios rust triples are there
+rustup target add aarch64-apple-ios
+rustup target add x86_64-apple-ios
+
+(cd ../../crypto_plugins/flutter_liblelantus/scripts/ios && ./build_all.sh ) &
+(cd ../../crypto_plugins/flutter_libepiccash/scripts/ios && ./build_all.sh )  &
+(cd ../../crypto_plugins/flutter_libmonero/scripts/ios/ && ./build_all.sh  ) &&
+set_rust_to_1720 &&
+(cd ../../crypto_plugins/frostdart/scripts/ios && ./build_all.sh ) &
+
+wait
+echo "Done building"
+
+# ensure ios rust triples are there
+rustup target add aarch64-apple-ios
+rustup target add x86_64-apple-ios
