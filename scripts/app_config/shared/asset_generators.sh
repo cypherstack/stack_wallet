230669bd97d86cd95f6121626abbd9f1c50535a7
julian <julian@cypherstack.com>
2024-05-22 11:41:49 -0600

    disable splash generation as the actual screen isn't used

--- scripts/app_config/shared/asset_generators.sh
+++ scripts/app_config/shared/asset_generators.sh
@@ -7,6 +7,7 @@ source ./env.sh
 # run icon and image generators
 pushd "${APP_PROJECT_ROOT_DIR}"
 flutter pub get
-dart run flutter_native_splash:create
+#native splash screen not used
+#dart run flutter_native_splash:create
 dart run flutter_launcher_icons
 popd
