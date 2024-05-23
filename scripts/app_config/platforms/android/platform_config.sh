933bfd9efd61861f3626ce750f0b12773ac48934
julian <julian@cypherstack.com>
2024-05-23 17:13:41 -0600

    android template files

--- scripts/app_config/platforms/android/platform_config.sh
+++ scripts/app_config/platforms/android/platform_config.sh
@@ -2,10 +2,29 @@
 
 set -x -e
 
-sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/android/app/build.gradle"
-sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/android/app/src/debug/AndroidManifest.xml"
-sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/android/app/src/profile/AndroidManifest.xml"
-sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/android/app/src/main/AndroidManifest.xml"
-sed -i "s/${APP_NAME_PLACEHOLDER}/${NEW_NAME}/g" "${APP_PROJECT_ROOT_DIR}/android/app/src/main/AndroidManifest.xml"
-sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/android/app/src/main/profile/AndroidManifest.xml"
-sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/android/app/src/main/kotlin/com/cypherstack/stackwallet/MainActivity.kt"
+F0="android/app/build.gradle"
+F1="android/app/src/debug/AndroidManifest.xml"
+F2="android/app/src/profile/AndroidManifest.xml"
+F3="android/app/src/main/AndroidManifest.xml"
+F4="android/app/src/main/profile/AndroidManifest.xml"
+F5="android/app/src/main/kotlin/com/cypherstack/stackwallet/MainActivity.kt"
+
+TEMPLATES="${APP_PROJECT_ROOT_DIR}/scripts/app_config/templates"
+
+for (( i=0; i<=5; i++ )); do
+  VAR="F${i}"
+  FILE="${APP_PROJECT_ROOT_DIR}/${!VAR}"
+  if [ -f "${FILE}" ]; then
+    rm "${FILE}"
+  fi
+  cp "${TEMPLATES}/${!VAR}" "${FILE}"
+done
+
+
+sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/${F0}"
+sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/${F1}"
+sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/${F2}"
+sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/${F3}"
+sed -i "s/${APP_NAME_PLACEHOLDER}/${NEW_NAME}/g" "${APP_PROJECT_ROOT_DIR}/${F3}"
+sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/${F4}"
+sed -i "s/${APP_ID_PLACEHOLDER}/${NEW_APP_ID}/g" "${APP_PROJECT_ROOT_DIR}/${F5}"
