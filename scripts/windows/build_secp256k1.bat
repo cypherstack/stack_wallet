e2f3a1ad797dacb6bfb9e7c1f024f142ff38f526
sneurlax <sneurlax@gmail.com>
2024-10-22 18:59:02 -0500

    track secp256k1 changes in build scripts and pin to latest commit

--- scripts/windows/build_secp256k1.bat
+++ scripts/windows/build_secp256k1.bat
@@ -2,6 +2,8 @@ if not exist "build" mkdir "build"
 cd build
 git clone https://github.com/bitcoin-core/secp256k1
 cd secp256k1
+git checkout 68b55209f1ba3e6c0417789598f5f75649e9c14c
+git reset --hard
 cmake -G "Visual Studio 17 2022" -A x64 -S . -B build
 cd build
 cmake --build .
