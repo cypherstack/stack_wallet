d77cfad36d8622296b631cb00b1fc68871f17a4e
julian <julian@cypherstack.com>
2024-10-23 10:30:20 -0600

    build scripts update

--- scripts/windows/build_all_duo.sh
+++ scripts/windows/build_all_duo.sh
@@ -11,7 +11,7 @@ set_rust_to_1671
 mkdir -p build
 (cd ../../crypto_plugins/flutter_libepiccash/scripts/windows && ./build_all.sh )
 (cd ../../crypto_plugins/flutter_liblelantus/scripts/windows && ./build_all.sh )
-(cd ../../crypto_plugins/cs_monero/tools/build_scripts && ./build_windows.sh )
+(cd ../../crypto_plugins/cs_monero/tools/build_scripts && ./build_windows.sh && ../copy_outputs_to_cs_monero_flutter_libs_package.sh  )
 set_rust_to_1720
 (cd ../../crypto_plugins/frostdart/scripts/windows && ./build_all.sh )
 
