e5dea740963a444c95482efae93da67342fb594d
sneurlax <sneurlax@gmail.com>
2024-12-30 19:50:25 -0600

    fix: override identifier
    WIP because this causes issues with BIP48Bitcoin not being in the configured list of supported coins as per AppConfig
    
    but we don't want it to appear as a distinct coin so that could be an issue

--- lib/wallets/crypto_currency/coins/bip48_bitcoin.dart
+++ lib/wallets/crypto_currency/coins/bip48_bitcoin.dart
@@ -1,8 +1,47 @@
 import '../../../utilities/enums/derive_path_type_enum.dart';
-import 'bitcoin.dart';
+import '../crypto_currency.dart';
 
 class BIP48Bitcoin extends Bitcoin {
-  BIP48Bitcoin(super.network);
+  BIP48Bitcoin(super.network) {
+    _idMain = "bip48Bitcoin";
+    _uriScheme = "bitcoin";
+    switch (network) {
+      case CryptoCurrencyNetwork.main:
+        _id = _idMain;
+        _name = "Bitcoin";
+        _ticker = "BTC";
+      case CryptoCurrencyNetwork.test:
+        _id = "bip48BitcoinTestNet";
+        _name = "tBitcoin";
+        _ticker = "tBTC";
+      case CryptoCurrencyNetwork.test4:
+        _id = "bip48BitcoinTestNet4";
+        _name = "t4Bitcoin";
+        _ticker = "t4BTC";
+      default:
+        throw Exception("Unsupported network: $network");
+    }
+  }
+
+  late final String _id;
+  @override
+  String get identifier => _id;
+
+  late final String _idMain;
+  @override
+  String get mainNetId => _idMain;
+
+  late final String _name;
+  @override
+  String get prettyName => _name;
+
+  late final String _uriScheme;
+  @override
+  String get uriScheme => _uriScheme;
+
+  late final String _ticker;
+  @override
+  String get ticker => _ticker;
 
   @override
   List<DerivePathType> get supportedDerivationPathTypes => [
