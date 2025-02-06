8ca4421c2a20beed0f8d7b2ade7edb62a8e84bd8
julian <julian@cypherstack.com>
2025-02-06 12:07:31 -0600

    adjust log level privacy warning text

--- lib/widgets/log_level_preference_widget.dart
+++ lib/widgets/log_level_preference_widget.dart
@@ -65,14 +65,16 @@ class _LogLevelPreferenceWidgetState
                 _levels[_sliderValue.toInt()];
           },
         ),
-        if (current == Level.debug || current == Level.trace)
+        if (current == Level.debug ||
+            current == Level.trace ||
+            current == Level.info)
           Padding(
             padding: const EdgeInsets.only(top: 10),
             child: RoundedContainer(
               color:
                   Theme.of(context).extension<StackColors>()!.warningBackground,
-              child: RichText(
-                text: TextSpan(
+              child: SelectableText.rich(
+                TextSpan(
                   text: "Privacy Warning: ",
                   style: STextStyles.label700(context).copyWith(
                     color: Theme.of(context)
@@ -119,11 +121,29 @@ class _LogLevelPreferenceWidgetState
                     ),
                     TextSpan(
                       text: " may log sensitive metadata, such as transaction"
-                          " details, addresses, or network activity. While no"
-                          " private keys, mnemonics, or credentials will ever "
-                          "be logged, enabling these levels could expose "
-                          "information that might compromise privacy if "
-                          "accessed by unauthorized parties.",
+                          " details, amounts, addresses, or network activity. While ",
+                      style: STextStyles.label(context).copyWith(
+                        color: Theme.of(context)
+                            .extension<StackColors>()!
+                            .warningForeground,
+                        fontSize: Util.isDesktop ? 14 : 12,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "Info",
+                      style: STextStyles.label700(context).copyWith(
+                        color: Theme.of(context)
+                            .extension<StackColors>()!
+                            .warningForeground,
+                        fontSize: Util.isDesktop ? 14 : 12,
+                      ),
+                    ),
+                    TextSpan(
+                      text: " logs are less likely to contain sensitive data, "
+                          "they may still include some. No private keys, "
+                          "mnemonics, or credentials will ever be logged, but "
+                          "enabling these levels could expose information that "
+                          "might compromise privacy if accessed by unauthorized parties.",
                       style: STextStyles.label(context).copyWith(
                         color: Theme.of(context)
                             .extension<StackColors>()!
