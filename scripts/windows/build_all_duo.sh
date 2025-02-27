448fd0c94d6b0be1d7889712f5a8d29c0181e36e
Tritonn204 <tritonn204@gmail.com>
2025-02-27 12:49:42 -0800

    keep version update without frostdart in windows build scripts

--- scripts/windows/build_all_duo.sh
+++ scripts/windows/build_all_duo.sh
@@ -11,6 +11,7 @@ set_rust_to_1671
 mkdir -p build
 (cd ../../crypto_plugins/flutter_libepiccash/scripts/windows && ./build_all.sh )
 (cd ../../crypto_plugins/flutter_liblelantus/scripts/windows && ./build_all.sh )
+set_rust_to_1720
 
 ./build_secp256k1_wsl.sh
 
