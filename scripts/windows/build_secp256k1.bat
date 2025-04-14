a4de7d34ccbe5c4cd9b5de3b8b5ad8a7a0452991
Tritonn204 <tritonn204@gmail.com>
2025-04-14 13:45:37 -0700

    windows secp script fix

--- scripts/windows/build_secp256k1.bat
+++ scripts/windows/build_secp256k1.bat
@@ -7,6 +7,6 @@ git reset --hard
 cmake -G "Visual Studio 17 2022" -A x64 -S . -B build
 cd build
 cmake --build .
-if not exist "..\..\..\..\build\" mkdir "..\..\..\..\..\build\"
-xcopy bin\Debug\libsecp256k1-2.dll "..\..\..\..\build\secp256k1.dll" /Y
+if not exist "..\..\..\..\..\build\" mkdir "..\..\..\..\..\build\"
+xcopy bin\Debug\libsecp256k1-2.dll "..\..\..\..\..\build\secp256k1.dll" /Y
 cd ..\..\..\
