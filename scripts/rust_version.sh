84073b62cf82e364a7646c22e7836cbdc7a2e493
sneurlax <sneurlax@gmail.com>
2025-09-04 16:36:29 -0500

    fix(mwc): fix rust versions script

--- scripts/rust_version.sh
+++ scripts/rust_version.sh
@@ -20,6 +20,15 @@ set_rust_version_for_libepiccash() {
 }
 
 set_rust_to_1810() {
+  if rustup toolchain list | grep -q "1.81.0"; then
+    rustup default 1.81.0
+  else
+    echo "Rust version 1.81.0 is not installed. Please install it using 'rustup install 1.81.0'." >&2
+    exit 1
+  fi
+}
+
+set_rust_to_1720() {
   if rustup toolchain list | grep -q "1.72.0"; then
     rustup default 1.72.0
   else
