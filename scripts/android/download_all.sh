f24724ce5070a284211df78127e356af1a3f3acb
Dan Miller <dan@cypherstack.com>
2026-04-29 22:12:44 -0700

    Add flutter_libmwc to download scripts

--- scripts/android/download_all.sh
+++ scripts/android/download_all.sh
@@ -9,10 +9,7 @@ PLUGINS_DIR=../../crypto_plugins
 
 (cd "${PLUGINS_DIR}"/flutter_libepiccash/scripts/android && ./download.sh)
 
-source ../rust_version.sh
-set_rust_version_for_libmwc
-(cd "${PLUGINS_DIR}"/flutter_libmwc/scripts/android && ./build_all.sh)
-set_rust_to_everything_else
+(cd "${PLUGINS_DIR}"/flutter_libmwc/scripts/android && ./download.sh)
 
 (cd "${PLUGINS_DIR}"/frostdart/scripts/android && ./build_all.sh)
 
--- scripts/linux/download_all.sh
+++ scripts/linux/download_all.sh
@@ -7,10 +7,7 @@ mkdir -p build
 
 (cd ../../crypto_plugins/flutter_libepiccash/scripts/linux && ./download.sh)
 
-source ../rust_version.sh
-set_rust_version_for_libmwc
-(cd ../../crypto_plugins/flutter_libmwc/scripts/linux && ./build_all.sh)
-set_rust_to_everything_else
+(cd ../../crypto_plugins/flutter_libmwc/scripts/linux && ./download.sh)
 
 (cd ../../crypto_plugins/frostdart/scripts/linux && ./build_all.sh)
 
